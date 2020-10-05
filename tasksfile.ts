89c09387436a4abfc9068bee694cdd2ad201b24e
suzulabo <41730006+suzulabo@users.noreply.github.com>
2020-10-05 14:18:43 +0900

    update

--- tasksfile.ts
+++ tasksfile.ts
@@ -1,18 +1,45 @@
 import { readFileSync, writeFileSync } from 'fs';
 import marked from 'marked';
+import { renderSync } from 'sass';
 import { cli, sh } from 'tasksfile';
+import { html_beautify } from 'js-beautify';
 
-const generate = () => {
+const fontURL =
+  'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&family=Open+Sans:wght@300&display=auto';
+
+const genHTML = () => {
   const md = readFileSync('index.md', 'utf8');
   const mdHtml = marked(md);
   const baseHtml = readFileSync('index.base.html', 'utf8');
   const outHtml = baseHtml.replace('@md@', mdHtml);
 
-  writeFileSync('public/index.html', outHtml, 'utf8');
+  const html = html_beautify(outHtml, { indent_size: 2 });
 
-  sh('npx sass index.scss public/index.css --no-source-map');
+  writeFileSync('public/index.html', html, 'utf8');
+};
+
+const genCSS = () => {
+  const scss = readFileSync('index.scss', 'utf8');
+  const res = renderSync({ data: scss });
+
+  const css = res.css
+    .toString('utf8')
+    .replace(
+      'http://fonts.googleapis.com/css?family=Open+Sans:300italic,300',
+      fontURL
+    );
+
+  writeFileSync('public/index.css', css, 'utf8');
+};
+
+const generate = () => {
+  sh('mkdir -p public');
+  genHTML();
+  genCSS();
 };
 
 cli({
   generate,
+  html: genHTML,
+  css: genCSS,
 });
